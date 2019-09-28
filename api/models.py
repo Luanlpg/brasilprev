19e13c8bbd75eb75b6751d81c7d305e0c7e0deb6
Pedro Henrique <pedro.progsantos@gmail.com>
2019-09-28 12:29:14 -0300

    Models

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class UserModel(models.Model):
     """=========================================================================
     Model de Usuário.
     ========================================================================="""
+    cpf = models.IntegerField(primary_key=True)
     username = models.CharField(max_length=60, unique=True)
     first_name = models.CharField(max_length=40, null=False)
     last_name = models.CharField(max_length=40, null=False)
@@ -15,7 +16,6 @@ class UserModel(models.Model):
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
     token = models.CharField(default='-- -- --', max_length=300)
-    cpf = models.IntegerField(max_length=11)
     city = models.CharField(max_length=120, null=True)
     district = models.CharField(max_length=150, null=True)
     complement = models.CharField(max_length=150, null=True)
@@ -33,3 +33,16 @@ class UserModel(models.Model):
         self.username = str(self.cpf)   
         self.token = uuid.uuid4().hex
         super(UserModel, self).save(*args, **kwargs)
+
+
+class AccountModel(models.Model):
+    cpf = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    conta = models.IntegerField()
+    saldo = models.FloatField()
+
+
+class TransactionModel(models.Model):
+    account_origin = models.ForeignKey(AccountModel, on_delete=models.PROTECT, related_name='origin')
+    account_destiny = models.ForeignKey(AccountModel, on_delete=models.PROTECT, related_name='destiny')
+    type_transactions = models.CharField(max_length=30)
+    value = models.FloatField()
